f92ab6c5b6f6f4dca06c0f6e79d42b931a4c3490
amit-k-umar <61649790+amit-k-umar@users.noreply.github.com>
2020-11-15 20:31:58 +0530

    little change

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cookieParser=require('cookie-parser')
 const {requireAuth}=require('./middleware/authMiddleware')
 const User = require("./models/userModel");
 const groupRouter = require('./routes/groupRoute');
+const PORT = process.env.PORT || 5000;
 // const GroupModel = require("./models/groupModel");
 require('dotenv').config()
 const app=express();
@@ -19,7 +20,7 @@ app.use(cookieParser());
 //database conection
 //const dbURI = 'mongodb+srv://wom:amit@123@merncalender.tpijb.mongodb.net/calender?retryWrites=true&w=majority';
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => {app.listen(5000); })
+  .then((result) => {app.listen(PORT); })
  
   .catch((err) => console.log(err));
   
